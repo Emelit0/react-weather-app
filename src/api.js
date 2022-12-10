481879878929dca79c4f620745a6af6498b47f66
Emelit0 <stollk811@gmail.com>
2022-12-10 19:04:55 +0100

    add enviroment variable

--- src/api.js
+++ src/api.js
@@ -1,7 +1,9 @@
+require("dotenv").config();
+
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "6f7cf38217msh9dbcab897ab118fp165544jsn835510c44ba9",
+    "X-RapidAPI-Key": process.env.GEO_API_KEY,
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
@@ -9,4 +11,4 @@ export const geoApiOptions = {
 export const geoApiUrl = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 export const weatherApiUrl = "https://api.openweathermap.org/data/2.5";
 
-export const weatherApiKey = "0cc393fcf2e9e8055751dd659daec137";
+export const weatherApiKey = process.env.OP_API_KEY;
